0ccbbecd1396c8bdde79883fa84be0c256f8047c
DESKTOP-AB8J0V3\PC_User <y24340216@gmail.com>
2025-01-30 11:56:53 +0900

    interfaceの実装

---
+++ Assets/TechC/Scripts/InGame/Interface/IDamageable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TechC.Interface
+{
+    public interface IDamageable
+    {
+        public interface IDamageable
+        {
+            void TakeDamage(float damage);
+            void Death();
+
+        }
+    }
+}
