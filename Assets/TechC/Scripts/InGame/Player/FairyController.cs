64726cf1ca2d52ebd290419fa9722d4c5cd2adab
22RENT-16\student <y24340216@gmail.com>
2025-02-01 16:50:42 +0900

    フェアリー追加

---
+++ Assets/TechC/Scripts/InGame/Player/FairyController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TechC
+{
+    public class FairyController : MonoBehaviour
+    {
+        [SerializeField] private Transform fairyPos;
+        [SerializeField] private Transform playerRoatate;
+        [SerializeField] private float moveSpeed = 1f;
+   
+
+        private void Update()
+        {
+            // フェアリーの位置がターゲット位置に向かって動く
+            transform.position = Vector3.Lerp(transform.position, fairyPos.position, Time.deltaTime * moveSpeed);
+
+           
+        }
+
+        private void LateUpdate()
+        {
+            transform.rotation = playerRoatate.rotation;
+        }
+
+    }
+}
